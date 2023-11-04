2d4b5c8ee12c28b357bebc44975fb7b43cf79403
devAayushOP <20bcs1076@cuchd.in>
2023-11-04 23:10:08 +0530

    server wale mein update kiye hai thoda sa

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 5001;
 
 dbConnect();
 app.use(express.json());
+app.get("/", (req, res) => {
+  return res.send({ msg: "server is up and running" });
+});
 app.use("/user", require("./routes/userRouter"));
 app.use("/movies", require("./routes/movieRouter"));
 app.listen(port, () => {
